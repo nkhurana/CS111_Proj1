5957fb2e59cae78b4cbc307529bb1878d9ecbddb
Yifeng Evan Shi <eshi@ucla.edu>
2013-04-20 13:33:48 -0700

    file redirection v2

--- execute-command.c
+++ execute-command.c
@@ -21,47 +21,45 @@ command_status (command_t c)
 void
 execute_command (command_t c, bool time_travel)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library. 
-  error (1, 0, "command execution not yet implemented");*/
- 
   char *cmd = c->u.word[0];
   pid_t pid;
   int status;
  
-  switch ( pid = fork() ) {
+  switch ( pid = fork() )
+  {
     case -1:
-      perror("fork()");
+      perror("Unable to fork");
       exit(EXIT_FAILURE);
     case 0: // in the child
 	  if (c->input)
 	  {
-	    int fd = open(c->input, O_RDONLY);
-		if (dup2(fd, 0) != 0);
-			perror("Unable to redirect input");
-		close(fd);
+	    int fdi = open(c->input, O_RDONLY);
+		if (dup2(fdi, 0) != 0)
+		  perror("Unable to redirect input");
+		close(fdi);
 	  }
 	  
 	  if (c->output)
 	  {
-	    int fd = open(c->output, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-		dup2(fd, 1);
-		close(fd);
+	    int fdo = open(c->output, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if (dup2(fdo, 1) != 1)
+		  perror("Unable to redirect output");
+		close(fdo);
 	  }
 	  
       status = execvp(cmd, c->u.word);
       exit(status); // only happens if execvp(2) fails
     default: // in parent
-      if ( waitpid(pid, &status, 0) < 0 ) {
-        perror("waitpid()");
+      if ( waitpid(pid, &status, 0) < 0 )
+	  {
+        perror("Waiting on child produced error");
         exit(EXIT_FAILURE);
       }
 
-      if ( WIFEXITED(status) ) {
+      if ( WIFEXITED(status) )
+	  {
         // return status from child
-        exit( WEXITSTATUS(status) );
+        c->status = WEXITSTATUS(status);
       }
-      exit(EXIT_FAILURE);
   }
 }
